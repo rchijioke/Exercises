d2eab6cf68c5df85e3bd0c4929c0e484f51dda74
Roy Chijioke <ekeneroy@gmail.com>
2023-02-10 15:09:41 -0600

    find key exercise

---
+++ lotide/findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+
+  if(actual === expected){
+   console.log("✅ ✅ ✅","Assertion Passed: " + actual +" === " + expected)
+  }
+  else return console.log("🛑 🛑 🛑", "Assertion Failed: " + actual+ " !== " + expected)
+  };
+
+  const findKey = function(object, callBack){
+  
+    for(let key in object){
+      if( callBack(object[key])){
+        return key
+      }
+    
+    }
+    return undefined
+  }
+  console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2) )// => "noma"
+ 
