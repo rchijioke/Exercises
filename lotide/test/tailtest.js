f517b47cf1561e9e4d98d40950e0f876e692d624
Roy Chijioke <ekeneroy@gmail.com>
2023-02-13 09:15:36 -0600

    mocha and chai middle, tail refactor

--- lotide/test/tailtest.js
+++ lotide/test/tailtest.js
@@ -1,5 +1,17 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const assert = require('chai').assert;
+
+
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns ['5'] for ['5']", () => {
+    assert.deepEqual(tail(['5']), ['5']);
+  })
+});
