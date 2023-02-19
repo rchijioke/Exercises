eb2c4897c7e27d7592eb0793cd1f46000a6fc785
Roy Chijioke <ekeneroy@gmail.com>
2023-02-19 16:18:22 -0600

    letterPositions.js

--- lotide/letterPositions.js
+++ lotide/letterPositions.js
@@ -1,22 +1,5 @@
-const assertArraysEqual = function (firstOne, secondOne){
-  if(eqArrays(first, second)){
-    console.log("Assertion Pass")
-  }
-  else{console.log("Assertion Failed")}
-}
-
-const eqArrays = function(first, second){
-  if(first.length !== second.length){
-    return false
-  }
-  for(let i=0; i<first.length; i++){
-    if (first[i] !== second[i]){
-      return false
-    }
-  }
-  return true
-}
-
+const assertArraysEqual = require("./assertArraysEqual")
+const eqArrays = require("./eqArrays")
 
 const letterPositions = function(sentence) {
   const results = {};
--- lotide/without.js
+++ lotide/without.js
@@ -20,5 +20,6 @@ const without = (source, itemsToRemove) => {
 
 }
 
+
 console.log(without([1, 2, 3], [1])) // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
