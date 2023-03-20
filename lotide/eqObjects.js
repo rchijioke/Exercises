a5dc61c988eaacfb61b335c3fb911505bc6b2347
Roy Chijioke <ekeneroy@gmail.com>
2023-03-20 18:02:16 -0500

    eqObjects.js updated

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -1,6 +1,13 @@
 const assertEqual = require("./assertEqual")
 const eqArrays = require("./assertEqual")
-
+/**
+ * Implement the definition for function eqObjects which will take in two objects 
+ * @param {*} object1 
+ * @param {*} object2 
+ * and returns true or false, based on a perfect match.
+ * @returns 
+ * 
+ */
 const eqObjects = function(object1, object2) {
 
   if (Object.keys(object1).length !== Object.keys(object2).length){
