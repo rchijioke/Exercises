12c46f15e042bc35ca57b3d910ee170fd8cc5d66
Roy Chijioke <ekeneroy@gmail.com>
2023-03-20 17:23:00 -0500

    edited assertEqual

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -4,6 +4,6 @@ const assertEqual = function(actual, expected) {
 if(actual === expected){
  console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`)
 }
-else return console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`)};
+else console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`)};
 
 module.exports = assertEqual;
