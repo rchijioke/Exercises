e7107b3905cac7491cf9f20ae3a85e49438c7c07
Roy Chijioke <ekeneroy@gmail.com>
2023-03-20 19:10:39 -0500

    findKeyByvalue update

--- lotide/findKeyByValue.js
+++ lotide/findKeyByValue.js
@@ -1,7 +1,12 @@
 const assertEqual = require("./assertEqual")
 
 
-
+/**
+ *  this code provides a simple and efficient way to find a key in an object based on a specified value.
+ * @param {*} objects: The function uses a for...in loop to iterate through each key in the object 
+ * @param {*} value: For each key, it checks if the corresponding value of that key matches the given value
+ * @returns If it does, the function returns the corresponding key. if it doesn's, it returns undefined
+ */
 const findKeyByValue = (objects, value) => {
 
   for (let key in objects) {
