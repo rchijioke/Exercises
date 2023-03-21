fbe6bab870564ac74268fac51c54f64958bbff5a
Roy Chijioke <ekeneroy@gmail.com>
2023-03-20 19:15:34 -0500

    flatten.js update

--- lotide/flatten.js
+++ lotide/flatten.js
@@ -1,43 +1,22 @@
-const assertArraysEqual = require("./assertArraysEqual")
-const eqArrays = require("./eqArrays")
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
 
-// const flatten = (flat1) => { 
-//  result = []
-//   for(let i; i < flat1.length; i++ ){
-//   if(Array.isArray(flat1[i])){
-//   return result.push(flat1.flat([i]))
-//   }
-
- 
-// }
-// }
+/**
+ * the code bellow provides a simple and effective way to flatten an array that may contain nested arrays
+ * @param {*} ary The function flatten takes an array (ary) as its parameter and
+ * @returns  returns a flattened version of the input array
+ */
 
 function flatten(ary) {
   var result = [];
-  for(var i = 0; i < ary.length; i++) {
-      if(Array.isArray(ary[i])) {
-          result = result.concat(flatten(ary[i]));
-      } else {
-          result.push(ary[i]);
-      }
+  for (var i = 0; i < ary.length; i++) {
+    if (Array.isArray(ary[i])) {
+      result = result.concat(flatten(ary[i]));
+    } else {
+      result.push(ary[i]);
+    }
   }
   return result;
 }
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  const flatten = [1, 2, [3, 4], 5, [6]]
-
-// console.log(flatten.flat())
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
